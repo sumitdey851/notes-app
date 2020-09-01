65ddd0dd2e923f2f3bfb4e8a27b4e7929591e9d4
Sumit Dey <sumitdey851@outlook.com>
2020-09-01 23:25:49 +0530

    Added documentation comments

--- app.js
+++ app.js
@@ -1,82 +1,82 @@
 //core-module packages
 
 //npm packages
-const chalk = require('chalk')
-const yargs = require('yargs')
+const chalk = require('chalk')                  //import chalk npm package for terminal output styling
+const yargs = require('yargs')                  //import yargs npm package to parse CLI arguments
 
 //own packages
-const notes = require('./notes.js')
+const notes = require('./notes.js')             //import javascript file containing functions to modify notes
 
 
 //customise yargs version
-yargs.version('1.1.0')
+yargs.version('1.1.0')                          //set the value shown by the --version option
 
 //create add command
-yargs.command({
-    command: 'add',
-    describe: 'Description: Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
+yargs.command({                                 //define the command for adding new notes
+    command: 'add',                             //name of command
+    describe: 'Description: Add a new note',    //info about the command
+    builder: {                                  //set options for the command
+        title: {                                //add a -title option
+            describe: 'Note title',             //info about the option
+            demandOption: true,                 //set the option as mandatory
+            type: 'string'                      //specify the data type of option value
+        },                                      //end of -title option definition
 
-        body: {
-            describe: 'The contents of your note',
-            demandOption: true,
-            type: 'string'
-        }
+        body: {                                 //add a -body option
+            describe: 'The contents of your note',  
+            demandOption: true,                 //set the option as mandatory
+            type: 'string'                      //specify the data type of the option value
+        }                                       //end of -body option definition
     },
-    handler(argv){
-        notes.addNote(argv.title, argv.body)
+    handler(argv){                              //function to handle option values
+        notes.addNote(argv.title, argv.body)    //calling the appropriate function to add a note
     }
 })
 
 
 //create remove command
-yargs.command({
-    command: 'remove',
-    describe: 'Remove an existing note',
-    builder: {
-        title: {
+yargs.command({                                 //define the command to remove existing notes
+    command: 'remove',                          //name of command
+    describe: 'Remove an existing note',        //info about command
+    builder: {                                  //set options for command
+        title: {                                //add a -title option
             describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler(argv){
-        notes.removeNote(argv.title)
+            demandOption: true,                 //set the option as mandatory
+            type: 'string'                      //data type of option value
+        }                                       //end of -title option definition
+    },                                          //end of builder
+    handler(argv){                              //function to handle option values
+        notes.removeNote(argv.title)            //calling appropriate function to remove a note
     }
 })
 
 //create read command
-yargs.command({
-    command: 'read',
-    describe: 'Open an existing note',
-    builder: {
-        title: {
+yargs.command({                                 //define the command to read existing notes
+    command: 'read',                            //command name
+    describe: 'Open an existing note',          //info about command
+    builder: {                                  //set options for command
+        title: {                                //add a -title option
             describe: 'Note title',
-            demandOption: true,
-            type: 'string' 
-        }
+            demandOption: true,                 //set option mandatory
+            type: 'string'                      //data type of option value
+        }                                       //end of -title option definition
     },
-    handler(argv){
-        notes.getNote(argv.title)
+    handler(argv){                              //function to handle option values
+        notes.getNote(argv.title)               //calling appropriate function to read a note
     }
 })
 
 //create list command
-yargs.command({
-    command: 'list',
-    describe: 'List the name of all the notes',
-    handler(){
+yargs.command({                                 //define a command to list all notes
+    command: 'list',                            //command name
+    describe: 'List the name of all the notes', //info about command
+    handler(){                                  //no options defined, just call an appropriate method to list notes
         notes.listNotes()
     }
 })
 
-if(!process.argv[2]){
+if(!process.argv[2]){                           //if no command used when file is run
     console.log('Try using --help to see a list of available commands')
 }
 
-yargs.parse()
+yargs.parse()                                   //call method to parse CLI arguments
--- notes.js
+++ notes.js
@@ -1,8 +1,8 @@
 const chalk = require('chalk')
-const fs = require('fs')
+const fs = require('fs')                            //import fs nodejs module for file-system operations
 
 
-const getNote = (title) => {
+const getNote = (title) => {                        //function to search and get the contents of a note using note title
     const notes = loadNotes()
     const readNote = notes.find((note) => note.title === title)
     
@@ -17,12 +17,15 @@ const getNote = (title) => {
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNote = notes.find((note) => note.title === title)
-
+    const duplicateNote = notes.find((note) => note.title === title)    //finding duplicate note title
+    //The find() method returns the value of the first element in an array that pass a test (provided as a function).
+    // The find() method executes the function once for each element present in the array:
+    // If it finds an array element where the function returns a true value, find() returns the value of that array element (and does not check the remaining values)
+    // Otherwise it returns undefined
     
 
     if(!duplicateNote){
-        notes.push({
+        notes.push({                //The push() method adds new items to the end of an array, and returns the new length.
             title: title,
             body: body
         })
@@ -38,8 +41,8 @@ const removeNote = (title) => {
     const notes = loadNotes()
     const flag = 0
     const notesToKeep = notes.filter((note) => note.title !== title)
-    
-    if(notes.length !== notesToKeep.length)
+    //The filter() method creates an array filled with all array elements that pass a test (provided as a function).
+    if(notes.length !== notesToKeep.length)             //length of original array not equal to filtered array
     {
         console.log(chalk.green.inverse('Note removed!'))
         saveNotes(notesToKeep)
@@ -49,32 +52,33 @@ const removeNote = (title) => {
 }
 
 const listNotes = () => {
-    const notes = loadNotes()
+    const notes = loadNotes()                           //get the notes in array form
     
     console.log(chalk.magenta.inverse('Your notes'))
     
-    notes.forEach((note) => console.log(note.title))
-    
+    notes.forEach((note) => console.log(note.title))    //use forEach() to cycle through the array objects and display the title property of each object
+    //The forEach() method calls a function once for each element in an array, in order.
 }
 
 
 
-const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
+const saveNotes = (notes) => {                          //receive the JavaScript object passed as an argument
+    const dataJSON = JSON.stringify(notes)              //convert value to JSON string
+    fs.writeFileSync('notes.json', dataJSON)            //write the json string to file
 }
 
 const loadNotes = () => {
     try{
-    const dataBuffer = fs.readFileSync('notes.json')
-    const dataJSON = dataBuffer.toString()
-    return JSON.parse(dataJSON)
+    const dataBuffer = fs.readFileSync('notes.json')    //get the byte values stored in file
+    const dataJSON = dataBuffer.toString()              //get a string representation of the byte values
+    return JSON.parse(dataJSON)                         //parse the string into JSON
     } catch(e){
-        return []
+        return []                                       //return empty array on error
     }
 }
 
-module.exports = {
+//module is a variable and exports is an object that export the functions for use in a different file
+module.exports = {                                      
     getNote: getNote,
     addNote: addNote,
     removeNote: removeNote,
